ccd80427a030008b546d89d9b42e85df74d1c1b3
ryanbas21 <ryanbas21@gmail.com>
2018-06-21 16:48:20 -0500

    Quick fixes

--- src/utils/index.test.tsx
+++ src/utils/index.test.tsx
@@ -10,18 +10,25 @@ test('Derive Posts', () => {
 				fields: {
 					content: 'Here is the content',
 					title: 'Blog Title'
-				}
+				},
+        sys: {
+
+          createdAt: '10/20',
+        }
 			},
 			{
 				fields: {
 					content: 'Here is the content',
 					title: 'Blog Title'
-				}
+				},
+        sys: {
+          createdAt: '10/21'
+        }
 			}
 		];
 	let expected = [
-		{ content: 'Here is the content', title: 'Blog Title' },
-		{ content: 'Here is the content', title: 'Blog Title' }
+		{ content: 'Here is the content', date: 'Oct 20, 2001 12:00 AM', title: 'Blog Title' },
+		{ content: 'Here is the content', date: 'Oct 21, 2001 12:00 AM', title: 'Blog Title' }
 	];
 	let actual = derivePosts(state);
 	expect(actual).toEqual(expected);
