160423731bcf26e91f65f622bd37fd956b8890a7
Ryan Bas <18267769+ryanbas21@users.noreply.github.com>
2018-07-28 01:27:52 -0400

    Prod fix (#7)
    *  with '#' will be ignored, and an empty message aborts the commit.
    
    * bug(rebase):
    
    removing temp tag file
    
    * prod(fix)
    quick fix for prod build
    updating gitignore and build for prod
    
    * bug(fix contentful deletion)
    
    quick udpate on missing deletion
    
    didn't delete a contentful import

---
+++ src/components/about.tsx
@@ -0,0 +1,18 @@
+import * as React from 'react';
+import { Divider } from 'semantic-ui-react';
+
+export default (props) => (
+	<div>
+		<p>
+			I'm currently a software engineer @ Paypal. I write
+			React/Redux/Node/Express with typescript to help support the Wallet at
+			your favorite payment platform.
+		</p>
+		<p>
+			I love functional programming, and you can see me write about it here. I
+			also have enjoyed using vim, so I may write down some of my thoughts here
+			as well.
+		</p>
+		<Divider />
+	</div>
+);
