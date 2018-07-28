bf9692703f7ceb8e08b53265f4e93deabe141d6a
ryanbas21 <ryanbas21@gmail.com>
2018-07-28 13:39:16 -0400

    bug(ssr?)
    SSR not working?
    
    Check

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import { renderToString } from 'react-dom/server';
+// import React from 'react';
+// import { renderToString } from 'react-dom/server';
 
-export const replaceRenderer = ({ bodyComponent, replaceBodyHTMLString }) => {
-	replaceBodyHTMLString(renderToString(<bodyComponent />));
-};
+// export const replaceRenderer = ({ bodyComponent, replaceBodyHTMLString }) => {
+// 	replaceBodyHTMLString(renderToString(<bodyComponent />));
+// };
