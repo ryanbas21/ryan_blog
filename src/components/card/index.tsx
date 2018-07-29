1fea688c0a9c7f6cf84f6d3de22fbecd0074ed76
ryanbas21 <ryanbas21@gmail.com>
2018-07-28 20:54:37 -0400

    feat(tags): Tags should now work
    Added tags to feed and refactored view logic

--- src/components/card/index.tsx
+++ src/components/card/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Card, Image } from 'semantic-ui-react';
 import Link from 'gatsby-link';
 import { length, slice } from 'ramda';
-import Tag from '../../components/tags/'../tags/;
+import Tag from '../../components/tags/';
 
 interface CardProps {
 	id: string;
@@ -16,15 +16,15 @@ interface CardProps {
 }
 
 const Cards: React.SFC<CardProps> = function CardsComp(props) {
-	const { content, tags } = props;
+	const { path, style, content, tags, media, title, date } = props;
 	const contentLength =
 		length(content) > 100 ? slice(0, 100, content) : 'Click to Read';
 	return (
-		<Card fluid style={props.style} as={Link} to={props.path}>
-			{props.media ? <Image src={props.media} /> : null}
+		<Card fluid style={style} as={Link} to={path}>
+			{media ? <Image src={media} /> : null}
 			<Card.Content>
-				<Card.Header>{props.title}</Card.Header>
-				<Card.Meta>{props.date}</Card.Meta>
+				<Card.Header content={title} />
+				<Card.Meta content={date} />
 				<Card.Description>
 					<div
 						dangerouslySetInnerHTML={{
@@ -32,10 +32,9 @@ const Cards: React.SFC<CardProps> = function CardsComp(props) {
 						}}
 					/>
 				</Card.Description>
-      <Card.Extra>
-          <Tags tags={tags} />
-      </Card.Extra>
-
+			</Card.Content>
+			<Card.Content extra>
+				<Tag tags={tags} />
 			</Card.Content>
 		</Card>
 	);
