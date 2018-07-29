c44f0154b717e52d3c3f0a52c52176a16dca99e2
ryanbas21 <ryanbas21@gmail.com>
2018-07-28 20:54:37 -0400

    feat(tags): Tags should now work
    Added tags to feed and refactored view logic

--- src/components/card/index.tsx
+++ src/components/card/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Card, Image } from 'semantic-ui-react';
 import Link from 'gatsby-link';
 import { length, slice } from 'ramda';
-import Tag from '../tags';
+import { Tag } from '../tags/tag';
 
 interface CardProps {
 	id: string;
