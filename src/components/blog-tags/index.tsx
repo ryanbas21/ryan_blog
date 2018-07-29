47de35c7baadaca68917743e2543c27795373d8c
ryanbas21 <ryanbas21@gmail.com>
2018-07-28 20:54:37 -0400

    feat(tags): Tags should now work
    Added tags to feed and refactored view logic

---
+++ src/components/blog-tags/index.tsx
@@ -0,0 +1,21 @@
+import * as React from 'react';
+import { Icon, Label } from 'semantic-ui-react';
+import { map, split } from 'ramda';
+import styles from './tag.module.css';
+
+interface TagProps {
+	tags: string[];
+}
+export const Tag: React.SFC<TagProps> = ({ tags }) => (
+	<div className={styles.spacing}>
+		{map(
+			(type, i) => (
+				<Label className={styles.spacing} key={type}>
+					<Icon name="tag" />
+					{type}
+				</Label>
+			),
+			split(',', tags)
+		)}
+	</div>
+);
