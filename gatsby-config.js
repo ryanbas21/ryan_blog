ca8e0abb13eb90dd2f6d640381efce1ee5010534
ryanbas21 <ryanbas21@gmail.com>
2020-02-10 21:21:12 -0700

    tracking clicks

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,33 @@ module.exports = {
 		siteUrl: `https://www.ryanbas.com`
 	},
 	plugins: [
+		{
+			resolve: `gatsby-plugin-google-analytics`,
+			options: {
+				// The property ID; the tracking code won't be generated without it
+				trackingId: process.env.G_TRACKING_ID,
+				// Defines where to place the tracking script - `true` in the head and `false` in the body
+				head: true,
+				// Setting this parameter is optional
+				anonymize: true,
+				// Setting this parameter is also optional
+				respectDNT: true,
+				// Avoids sending pageview hits from custom paths
+				exclude: ['/preview/**'],
+				// Delays sending pageview hits on route update (in milliseconds)
+				pageTransitionDelay: 0,
+				// Enables Google Optimize using your container Id
+				// optimizeId: process.env.G_OPTIMIZE_ID,
+				// // Enables Google Optimize Experiment ID
+				// experimentId: 'YOUR_GOOGLE_EXPERIMENT_ID',
+				// // Set Variation ID. 0 for original 1,2,3....
+				// variationId: 'YOUR_GOOGLE_OPTIMIZE_VARIATION_ID',
+				// Any additional optional fields
+				sampleRate: 5,
+				siteSpeedSampleRate: 10,
+				cookieDomain: 'example.com'
+			}
+		},
 		`gatsby-plugin-sharp`,
 		`gatsby-plugin-postcss`,
 		`gatsby-plugin-react-helmet`,
@@ -26,41 +53,9 @@ module.exports = {
 			}
 		},
 		{
-			resolve: `gatsby-plugin-google-analytics`,
-			options: {
-				// The property ID; the tracking code won't be generated without it
-				trackingId: process.env.G_TRACKING_ID,
-				// Defines where to place the tracking script - `true` in the head and `false` in the body
-				head: false,
-				// Setting this parameter is optional
-				anonymize: true,
-				// Setting this parameter is also optional
-				respectDNT: true,
-				// Avoids sending pageview hits from custom paths
-				exclude: ['/preview/**'],
-				// Delays sending pageview hits on route update (in milliseconds)
-				pageTransitionDelay: 0,
-				// Enables Google Optimize using your container Id
-				// optimizeId: process.env.G_OPTIMIZE_ID,
-				// // Enables Google Optimize Experiment ID
-				// experimentId: 'YOUR_GOOGLE_EXPERIMENT_ID',
-				// // Set Variation ID. 0 for original 1,2,3....
-				// variationId: 'YOUR_GOOGLE_OPTIMIZE_VARIATION_ID',
-				// Any additional optional fields
-				sampleRate: 5,
-				siteSpeedSampleRate: 10,
-				cookieDomain: 'example.com'
-			}
+			resolve: '@bundle-analyzer/gatsby-plugin',
+			options: { token: process.env.BUNDLE_ANALYZER_TOKEN }
 		},
-		// {
-		// 	resolve: `gatsby-source-contentful`,
-		// 	options: {
-		// 		spaceId: process.env.SPACE_ID,
-		// 		// Learn about environment variables: https://gatsby.dev/env-vars
-		// 		accessToken: process.env.CONTENTFUL_PREVIEW_ACCESS_TOKEN,
-		// 		host: `preview.contentful.com`
-		// 	}
-		// },
 		{
 			resolve: 'gatsby-plugin-robots-txt',
 			options: {
